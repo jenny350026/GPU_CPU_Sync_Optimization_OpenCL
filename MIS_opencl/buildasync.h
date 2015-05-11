256d0be4aee53c20fd02ac4e27030b695444de58
root <zaddan>
2015-05-11 12:59:54 -0500

    sync cleanup

--- MIS_opencl/buildasync.h
+++ MIS_opencl/buildasync.h
@@ -3,12 +3,12 @@
 export AMDAPPSDKROOT="/opt/AMDAPPSDK-3.0-0-Beta"
 export LD_LIBRARY_PATH="/opt/AMDAPPSDK-3.0-0-Beta/lib/x86_64/"
 
-echo "Removing previous MIS_async.o"
-if [ -f MIS_async.o ] ; then rm MIS_async.o ; fi
-echo "g++ -o MIS_async.o -c MIS_async.cpp -I$AMDAPPSDKROOT/include"
-g++ -o MIS_async.o -c MIS_async.cpp -I ./ -I$AMDAPPSDKROOT/include 
+echo "Removing previous MIS_sync.o"
+if [ -f MIS_sync.o ] ; then rm MIS_sync.o ; fi
+echo "g++ -o MIS_sync.o -c MIS_sync.cpp -I$AMDAPPSDKROOT/include"
+g++ -o MIS_sync.o -c MIS_sync.cpp -I ./ -I$AMDAPPSDKROOT/include 
 
-echo "Removing previous executable MIS_async" 
-if [ -f MIS_async ] ; then rm MIS_async ; fi
-echo "g++ -o MIS_async MIS_async.o -L$AMDAPPSDKROOT/lib/x86_64"
-g++ -o MIS_async MIS_async.o -lOpenCL -L$AMDAPPSDKROOT/lib/x86_64
+echo "Removing previous executable MIS_sync" 
+if [ -f MIS_sync ] ; then rm MIS_sync ; fi
+echo "g++ -o MIS_sync MIS_sync.o -L$AMDAPPSDKROOT/lib/x86_64"
+g++ -o MIS_sync MIS_sync.o -lOpenCL -L$AMDAPPSDKROOT/lib/x86_64
